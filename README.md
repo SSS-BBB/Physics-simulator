a79aa2dbe473f4327dc62fdf253250e477bc4b3e
SSS BBB <sssbbbkaow@gmail.com>
2023-08-21 21:30:33 +0700

    Acceleration

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Physics-simulator
 We will simulate basic physics using python with library pygame to show the graphic, pymunk to calculate the physics, and matplotlib to show the graph after the simulation.
+
+# Acceralation
+We will simulate a square moving with acceleration, and see the graph. (There is only the acceleration in the x-axis)
+[alt text](https://github.com/[username]/[reponame]/blob/[branch]/velocity with acceleration.jpg?raw=true)
+This is a velocity graph as you can see acceleration make the velocity larger in a linear way because the acceleration is constant.
+[alt text](https://github.com/[username]/[reponame]/blob/[branch]/position with acceleration.jpg?raw=true)
+How ever the position is not linear that is because the longer it takes the faster it gets, basically you can move faster as the time goes by. That is why the position is curve but not linear.
--- main.py
+++ main.py
@@ -13,24 +13,35 @@ FPS = 60
 space = pymunk.Space()
 space.gravity = 0, -981
 
-class create_square:
-    def __init__(self, position, width, height, density, velocity, color):
-        self.init_pos = position
-        self.width = width
-        self.height = height
+class physics_obj:
+    def __init__(self, init_position, init_velocity, acceralation, color):
+        self.init_position = init_position
+        self.init_velocity = init_velocity
+        self.acceralation = acceralation
         self.color = color
-        self.density = density
-        self.init_velocity = velocity
 
         self.body = pymunk.Body()
-        self.body.position = position
-        self.body.velocity = velocity
+        self.body.position = init_position
+        self.body.velocity = init_velocity
+
+class create_square(physics_obj):
+    def __init__(self, init_position=(WIDTH/2, HEIGHT/2), width=50, height=50, density=1, init_velocity=(0, 0), 
+                 color=(0, 0, 255), collision_type=1, acceralation=(5, 0)):
+        
+        super().__init__(init_position, init_velocity, acceralation, color)
+        self.width = width
+        self.height = height
+        self.density = density
+
         self.shape = pymunk.Poly.create_box(self.body, (width, height))
         self.shape.density = density
+        self.shape.elasticity = 1
+        self.shape.collision_type = collision_type
+
         space.add(self.body, self.shape)
 
 class segment_obj:
-    def __init__(self, start_pos, end_pos, thickness, color):
+    def __init__(self, start_pos=(0, 10), end_pos=(WIDTH, 10), thickness=5, color=(0, 0, 0)):
         # init variable
         self.body = pymunk.Body(body_type=pymunk.Body.STATIC)
 
@@ -39,11 +50,33 @@ class segment_obj:
         self.color = color
 
         self.shape = pymunk.Segment(self.body, start_pos, end_pos, thickness)
-        self.shape.elasticity = 1
+        # self.shape.elasticity = 1
         space.add(self.body, self.shape)
 
-stupid_square1 = create_square((20, HEIGHT/2), 50, 50, 1, (100, 0), (0, 0, 255))
-stupid_segment = segment_obj((0, 50), (WIDTH, 50), 5, (0, 0, 0))
+class graph_info:
+    def __init__(self):
+        self.pos_x = []
+        self.pos_y = []
+        self.vel_x = []
+        self.vel_y = []
+
+    def add_info(self, obj: physics_obj):
+        self.pos_x.append(obj.body.position[0])
+        self.pos_y.append(obj.body.position[1])
+        self.vel_x.append(obj.body.velocity[0])
+        self.vel_y.append(obj.body.velocity[1])
+
+obj_list = {"square": [], "circle": []}
+
+squares = []
+stupid_square1 = create_square((20, 50+2), 50, 50, 1, (100, 0), (0, 0, 255), collision_type=1)
+squares.append(stupid_square1)
+# stupid_square2 = create_square((WIDTH-100, HEIGHT/2), 50, 50, 1, (-100, 0), (255, 0, 0), collision_type=2)
+# squares.append(stupid_square2)
+obj_list["square"] = squares
+
+stupid_segment = segment_obj((0, 50), (WIDTH*100, 50), 5, (0, 0, 0))
+
 
 def convert_coordinates(point):
     # we need to convert because pygame and pymunk coordinates system don't work the same way.
@@ -67,34 +100,46 @@ def draw_segment(display, segment: segment_obj):
     end = convert_coordinates(segment.init_pos[1])
     pygame.draw.line(display, segment.color, start, end, segment.thickness)
 
-def show_graph(t, pos):
+def show_graph(t, graph_info: graph_info):
     # decoration
     plt.xlabel("time")
-    plt.ylabel("position")
+    plt.ylabel("postion")
 
     # graph
-    plt.plot(t, pos, label="position with respect to time")
+    plt.plot(t, graph_info.pos_x, label="pos_x")
+    plt.plot(t, graph_info.pos_y, label="pos_y")
 
     # show
     plt.legend()
     plt.show()
 
+def collide(arbiter, space, data):
+    print("collided")
+
+def update_obj(obj: physics_obj, t):
+    vel_x = obj.body.velocity[0] + obj.acceralation[0]
+    vel_y = obj.body.velocity[1] + obj.acceralation[1]
+    obj.body.velocity = (vel_x, vel_y)
+
 def game():
     # data to show to the grpah
     time_list = []
-    square_x = []
-    square_y = []
     time_now = 0
+
+    handler = space.add_collision_handler(1, 2)
+    handler.separate = collide
+
+    square_graph = graph_info()
     while not_exit(pygame.event.get()):
         display.fill((255, 255, 255))
 
         # drawing
-        draw_square(display, stupid_square1)
+        for s in squares:
+            draw_square(display, s)
         draw_segment(display, stupid_segment)
 
         # update data to show to the grpah
-        square_x.append(stupid_square1.body.position[0])
-        square_y.append(stupid_square1.body.position[1])
+        square_graph.add_info(stupid_square1)
         time_list.append(time_now)
 
         # update screen
@@ -105,9 +150,12 @@ def game():
 
         # 1/FPS seconds for each loop
         space.step(1/FPS)
+        for obj in obj_list.values():
+            for o in obj:
+                update_obj(o, 1/FPS)
         time_now += 1/FPS
 
     pygame.quit()
-    show_graph(time_list, square_y)
+    show_graph(time_list, square_graph)
 
 game()
